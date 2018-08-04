8cac4fbcf30c8ac90f071f4d2086a4541eb88f8f
Castracani <willmyers993@gmail.com>
2018-08-04 18:28:08 -0400

    user input doesn't break table generation finally

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table2");
 
-var showTable = require("./tableConstructor.js");
+var displayTable = require("./tableConstructor.js");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -18,16 +18,37 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
 });
 
-var displayForUser = function() {
-  var display = new showTable();
-  connection.query("SELECT * FROM products", function(err, results) {
+var displayForUser = function () {
+  var display = new displayTable();
+  connection.query('SELECT * FROM products', function (err, results) {
     display.displayInventoryTable(results);
-  })
-};
+    itemPurchase();
+  });
+}
 
+var itemPurchase = function () {
+  console.log("\n");
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter the ID of the item you would like to purchase from us."
+    },
+    {
+      type: "input",
+      name: "quantity",
+      message: "Please enter the amount you would like to purchase."
+    }
+  ])
+    .then(function (answer) {
+      connection.query("SELECT product_name, department_name, price, stock_quantity FROM products WHERE ?", { item_id: answer.id }, function (err, res) {
+        console.log('\n  So, you would like to buy ' + answer.quantity + ' ' + res[0].product_name + ' at $' + res[0].price + ' each, right?');
+      })
+    })
+};
 displayForUser();
